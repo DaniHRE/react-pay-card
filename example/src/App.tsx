9227921a4b7d869ae524cbb38c6e57ee8eab0696
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-16 11:24:52 -0300

    feat: add example folder

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import Card from 'react-card-bank/lib/components/Card';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card cardHolder={''} cardNumber={''} cardMonth={''} cardYear={''} cardCvv={''} isCardFlipped={false} onUpdateState={function (keyName: any, value: any): void {
+        throw new Error('Function not implemented.');
+      } } />
+    </div>
+  )
+}
+
+export default App
