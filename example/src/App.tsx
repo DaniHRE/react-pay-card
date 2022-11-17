d3b3a45e5baecbb3e2fbb35d38da6eee8a8df183
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-17 11:30:58 -0300

    update: mock default values

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="App">
-      <Card cardHolder={''} cardNumber={''} cardMonth={''} cardYear={''} cardCvv={''} isCardFlipped={false} onUpdateState={function (keyName: any, value: any): void {
+      <Card cardHolder={'JOHN DOE'} cardNumber={'5552 0854 0525 5586'} cardMonth={'12'} cardYear={'2022'} cardCvv={'000'} isCardFlipped={false} onUpdateState={function (keyName: any, value: any): void {
         throw new Error('Function not implemented.');
       } } />
     </div>
