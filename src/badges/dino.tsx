323070e703ab0a97730767ce4b5f8741e3c4ea03
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-18 11:31:49 -0300

    fix(svg): enable-background propertie

--- src/badges/dino.tsx
+++ src/badges/dino.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Dino() {
     return (
         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            width="100%" viewBox="0 0 184 184" enable-background="new 0 0 184 184" xmlSpace="preserve">
+            width="100%" viewBox="0 0 184 184" enableBackground="new 0 0 184 184" xmlSpace="preserve">
             <path opacity="0" stroke="none"
                 d="
 M1.000000,64.000000 
