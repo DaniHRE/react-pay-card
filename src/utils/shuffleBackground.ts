f325f80c2da90b117675bcd6ea46600d68144407
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-18 11:32:46 -0300

    feat(utils): randomize background util

---
+++ src/utils/shuffleBackground.ts
@@ -0,0 +1,13 @@
+import * as dino from '../assets/card-type/dino.png';
+
+const BACKGROUNDS = [
+    dino,
+]
+
+export const data = console.log(dino);
+
+export function shuffleBackground() {
+    return (
+        data
+    );
+};
