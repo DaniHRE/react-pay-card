a7e464265e395f3598a3743433676817a6daaae8
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-18 11:31:22 -0300

    fix: example page card mock values

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,16 @@
-import { Card } from 'react-card-bank';
+import { Card } from '../../lib/index';
 
 function App() {
 
   return (
     <div className="App">
-      <Card cardHolder={'JOHN DOE'} cardNumber={'5552 0854 0525 5586'} cardMonth={'12'} cardYear={'2022'} cardCvv={'000'} isCardFlipped={false} onUpdateState={function (keyName: any, value: any): void {
-        throw new Error('Function not implemented.');
-      } } />
+      <Card
+        cardHolder={'JOHN DOE'}
+        cardNumber={'0037 0854 0525 5006'}
+        cardMonth={'10'}
+        cardYear={'2022'}
+        cardCvv={'000'}
+      />
     </div>
   )
 }
