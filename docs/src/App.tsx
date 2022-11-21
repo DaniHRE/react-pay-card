e5b9d02fb148ea43d02d7b93c00d11643e72797d
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-21 11:42:37 +0000

    fix(docs): gitignore

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,17 +1,33 @@
-import { Card } from 'react-pay-card';
+import { Card, SimpleFormCard } from 'react-pay-card';
+import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage';
 import './App.css';
 
 function App() {
 
   return (
     <div className="card">
-      <Card
-        cardHolder={'JOHN DOE'}
-        cardNumber={'5037 0854 0525 5006'}
-        cardMonth={'10'}
-        cardYear={'2022'}
-        cardCvv={'000'}
-      />
+      <Fullpage>
+        <FullPageSections>
+
+          <FullpageSection>
+            <div className="card">
+              <Card
+                cardHolder={'JOHN DOE'}
+                cardNumber={'5037 0854 0525 5006'}
+                cardMonth={'10'}
+                cardYear={'2022'}
+                cardCvv={'000'}
+              />
+            </div>
+          </FullpageSection>
+
+          <FullpageSection>
+            <SimpleFormCard />
+          </FullpageSection>
+
+        </FullPageSections>
+      </Fullpage>
+
     </div>
   )
 }
