8ae291ce220170d585913c1a0d1545dac43797db
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-21 11:14:40 +0000

    feat(docs): add example site

---
+++ docs/src/App.tsx
@@ -0,0 +1,19 @@
+import { Card } from 'react-pay-card';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="card">
+      <Card
+        cardHolder={'JOHN DOE'}
+        cardNumber={'5037 0854 0525 5006'}
+        cardMonth={'10'}
+        cardYear={'2022'}
+        cardCvv={'000'}
+      />
+    </div>
+  )
+}
+
+export default App
